7461c1bc7ae9654e8d223aa51fbaf4c69047994c
com.rapollon-developer <ys.heo.korea@gmail.com>
2023-01-19 10:59:16 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello React
+        </p>
+        <p>Pipeline 구축하기</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
